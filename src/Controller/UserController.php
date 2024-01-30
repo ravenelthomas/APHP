8aca6625b1891fb3407ca760b951fe180ea89f11
Thomas Ravenel <ravenel.thomas@outlook.fr>
2024-01-30 12:34:29 +0100

    security fixe and admin panel work

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,47 +4,92 @@ namespace App\Controller;
 
 
 use App\Repository\CalendarRepository;
+use App\Entity\Calendar;
+use App\Form\CalendarType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    
+
     // private $calendarRepository;
 
     // public function __construct(CalendarRepository $calendarRepository)
     // {
     //     $this->calendarRepository = $calendarRepository;
     // }
-    
+
     public function __construct()
     {
-        
+
     }
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(CalendarRepository $calendarRepository): Response
     {
         $events = $calendarRepository->findByIdUser($this->getUser()->getId());
+        $name = $this->getUser()->getName();
+        $surname = $this->getUser()->getSurname();
         $rdvs = [];
-        foreach($events as $event){
-            $rdvs = [
-                'id' => $event->getId(),
-                'start' => $event->getStart()->format('Y-m-d H:i:s'),
-                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
-                'title' => $event->getTitle(),
-                'description' => $event->getDescription(),
-                'categories' => $event->getCategories(),
+        if(sizeof($events) > 0){
+            foreach ($events as $event) {
+                $rdvs[] = [
+                    'id' => $event->getId(),
+                    'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                    'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                    'title' => $event->getTitle(),
+                    'description' => $event->getDescription(),
+                    'categories' => $event->getCategories(),
+                    'name' => $name,
+                    'surname' => $surname,
+                ];
+            }
+        }
+        else{
+            $rdvs[] = [
+                'id' => '',
+                'start' => '',
+                'end' => '',
+                'title' => '',
+                'description' => '',
+                'categories' => '',
+                'name' => $name,
+                'surname' => $surname,
             ];
         }
-        $data = json_encode($rdvs);
-        return $this->render('user/index.html.twig', 
+            
+        $data = $rdvs;
+        return $this->render(
+            'user/index.html.twig',
             compact('data'),
-        
-        );
-    
-    }
+            
 
+        );
+}
+#[Route('/calendarAdd', name: 'app_user_calendar_add', methods: ['GET', 'POST'])]
+    public function addUserCalendar(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $calendar = new Calendar();
+        $form = $this->createForm(CalendarType::class, $calendar, [
+            'isAdmin' => in_array('ROLE_ADMIN', $this->getUser()->getRoles()),
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($calendar);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_calendar_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('calendar/newUser.html.twig', [
+            'calendar' => $calendar,
+            'form' => $form,
+            'user' => $this->getUser(),
+        ]);
+    }
 }
 
--- src/Form/CalendarType.php
+++ src/Form/CalendarType.php
@@ -4,39 +4,60 @@ namespace App\Form;
 
 use App\Entity\Calendar;
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
-
+use Symfony\Component\Security\Core\Security;
 class CalendarType extends AbstractType
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $userName = function ($user) {
-            return $user->getName() . ' ' . $user->getSurname();
-        };
         $builder
             ->add('start_')
             ->add('end_')
             ->add('title')
             ->add('description')
-            ->add('categories')
-            ->add('user', EntityType::class, [
+            ->add('categories');
+
+        // Obtenez l'utilisateur connecté
+        $user = $this->security->getUser();
+
+        if (!$options['isAdmin']) {
+            // Si l'utilisateur n'est pas administrateur, affichez uniquement son propre nom
+            $builder->add('user', TextType::class, [
+                'disabled' => true,
+                'data' => $user->getName() . ' ' . $user->getSurname(),
+                'label' => 'User',
+            ]);
+        } else {
+            // Si l'utilisateur est administrateur, affichez la liste complète des utilisateurs
+            $builder->add('user', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => $userName,
+                'choice_label' => function ($user) {
+                    return $user->getName() . ' ' . $user->getSurname();
+                },
                 'multiple' => false,
                 'expanded' => false,
                 'label' => 'User',
                 'required' => true,
             ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Calendar::class,
+            'isAdmin' => false,
         ]);
     }
 }
